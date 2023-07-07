8880c60dc7db1ac2afb5eccaa9f3dcd16162ebca
Zack <htunyarzar2001@gmail.com>
2023-07-07 14:57:14 +0630

    added querystring

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const http = require('http')
 const url = require('url');
+const qs = require('querystring')
+
 require('dotenv').config()
+
+
+const responder = (req,res,param)=>{
+    res.writeHead(200,{"Content-Type":"text/html"})
+    res.end(param)
+}
+
 const routes = {
     "GET":{
-        "/": (req,res,params)=>{
-            res.writeHead(200,{"Content-Type":"text/html"})
-            res.end("<h1>Get Method / route </h1>")
+        "/": (req,res)=>{
+            responder(req,res,`<h1>Get Method / route </h1>`)
         },
-        "/home":(req,res,params)=> {
-            res.writeHead(200,{"Content-Type":"text/html"})
-            res.end(`<h1>Get Method /home route with params of ${params.query.name} and ${params.query.age} </h1>`)
+        "/home":(req,res)=> {
+            
         }
     },
     "POST":{
-        "/": (req,res,params)=>{
-            res.writeHead(200,{"Content-Type":"text/html"})
-            res.end("<h1>Post Method / route </h1>")
+        "/": (req,res)=>{
+            responder(req,res,`<h1>Post Method / route </h1>`)
         },
-        "/about":(req,res,params)=> {
-            res.writeHead(200,{"Content-Type":"text/html"})
-            res.end("<h1>Post Method /about route </h1>")
+        "/api/login":(req,res)=> {
+            let body ='';
+           req.on('data',data=>{
+            body +=data
+           })
+           req.on('end',()=>{
+            let  query = qs.parse(body)
+            console.log(query);
+            res.end();
+           })
         }
     },
-    "NA":(req,res,params)=>{
-            res.writeHead(404);
-            res.end("<h1>No Page for that Route</h1>")
+    "NA":(req,res)=>{
+            responder(req,res,`<h1>No Page for that Route</h1>`)
     }
 
 }
@@ -32,11 +44,12 @@ const routes = {
  const startFunction = (req,res)=>{
 
     const reqMethod = req.method;
+
     const params =url.parse(req.url,true) ;
-    const name = params.query.name;
-    const age = params.query.age;
-    console.log(name)
-    console.log(age)
+    // const name = params.query.name;
+    // const age = params.query.age;
+    // console.log(name)
+    // console.log(age)
 
    const resoverRoute =  routes[reqMethod][params.pathname];
 //    console.log(resoverRoute)
@@ -51,7 +64,7 @@ const routes = {
 const server = http.createServer(startFunction)
 
 server.listen(process.env.PORT,()=>{
-    console.log(`Server is running ${process.env.PORT}!`)
+    console.log(`Server is running ${process.env.PO}!`)
 })
 
 
